1420da072a4bbf8aca6a361fbadef4e17cc71101
Kalliojumala <joel.kallio@gmail.com>
2022-10-26 10:46:54 +0300

    4.4
    totalLikes function and 3 tests for it.

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,7 +1,9 @@
-const dummy = require('../utils/test_functions')
+const dummy = require('../utils/test_functions').dummy
 
 test('dummy returns one', () => {
     const blogs = []
     const result = dummy(blogs)
     expect(result).toBe(1)
 })
+
+
--- utils/test_functions.js
+++ utils/test_functions.js
@@ -2,4 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = dummy
+const totalLikes = (blogs) => {
+    let likes = 0
+    blogs.forEach(blog => {
+        likes += blog.likes
+    });
+
+    return likes
+}
+
+module.exports = {dummy, totalLikes}
