5ff41e186f350b5d40bb1bccc9e8c12b4270582c
Kalliojumala <joel.kallio@gmail.com>
2022-10-27 11:43:07 +0300

    4.10
    - Test http post request against live MongoDB DONE
    - Refactor route for async/await DONE

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,14 +7,12 @@ blogRouter.get('/api/blogs', async (request, response) => {
   })
   
 
-blogRouter.post('/api/blogs', (request, response) => {
+blogRouter.post('/api/blogs', async (request, response) => {
     const blog = new Blog(request.body)
   
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const result = await blog.save()
+    response.status(201).json(result)
+      
   })
 
 module.exports = blogRouter
