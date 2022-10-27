5b6cf6d124a92afdb4aef46c873e34f28a40711f
Kalliojumala <joel.kallio@gmail.com>
2022-10-27 10:56:10 +0300

    4.8 Tasks done
    - Setup test env
    - Test http get request with SuperTest against live MongoDB database
    - Refactor get route to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,9 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog_schema')
 
-blogRouter.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogRouter.get('/api/blogs', async (request, response) => {
+    const blogEntries = await Blog.find({})
+    response.json(blogEntries)
   })
   
 
---
+++ tests/blogApi.test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const initialData = require('./testData').blogsMultiple
+const Blog = require('../models/blog_schema')
+
+//Clear test db before tests to maintain same database state
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+    //Create Blog-objects from initial data
+    const blogObjects = initialData.map((blog) => new Blog(blog))
+
+    //Blog.save() returns a promise
+    const arr = blogObjects.map((blog) => blog.save())
+
+    //Promise.all(array) waits for all blogObjects to be saved to mongo
+    await Promise.all(arr)
+})
+
+test('http get returns correct amount of JSON-formatted blog entries', 
+    async () => {
+        const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        
+        expect(response.body).toHaveLength(initialData.length)
+        
+    })
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
